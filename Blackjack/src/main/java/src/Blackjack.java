0161c1c8c8a50371f63d4e450494cabc63395e83
DESKTOP-CJ9OI5G\Szejk <rafajk@wp.pl>
2017-06-20 16:17:18 +0200

    Dodanie przycisku nowa gra

--- Blackjack/src/main/java/src/Blackjack.java
+++ Blackjack/src/main/java/src/Blackjack.java
@@ -1,11 +1,47 @@
 package src;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 public class Blackjack {
 	
+	
+	
 	private static JFrame frame = new MainFrame();
+	
+	
+	
+	private static JButton btnNewGame;
+	
+	
+	
+	
+	public static void initGuiObjects() {
+		btnNewGame = new JButton("New Game"); // przycisk nowa gra
+		btnNewGame.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) { // co się dzieje po wcisnieciu nowa gra
+				//newGame(); 
+			}
+		});
+		
+		btnNewGame.setBounds(20, 610, 99, 50);
+		frame.getContentPane().add(btnNewGame);
+	}
+	
+	
+		
+	
 	public static void main(String[] args){
+
+		
+		initGuiObjects();
 		frame.setVisible(true);
+		
 	}
 }
