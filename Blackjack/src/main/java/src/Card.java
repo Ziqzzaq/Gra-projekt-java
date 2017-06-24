a3ffc9c008dbe6b9ead90a47c5368b4ade7b2b14
Michał Borzęcki <ziqzzaq.web@gmail.com>
2017-06-24 19:46:19 +0200

    Dodanie konstruktora i inicjalizacja wartości

--- Blackjack/src/main/java/src/Card.java
+++ Blackjack/src/main/java/src/Card.java
@@ -1,5 +1,13 @@
 package src;
 
-public class Card {
-
+public class Card { // Ta klasa jest tworzona dla każdej karty i przechowuje informacje o niej
+	// Zmienne do przechowywania rangi, koloru i wartości karty 
+		public String rank = "", suit = "";
+		public int value = 0;
+		
+		Card(String r, String s, int v) { // Konstruktor - inicjalizacja wartości
+			this.rank = r;
+			this.suit = s;
+			this.value = v;
+		}
 }
