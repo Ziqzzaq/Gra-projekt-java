8ee47d0886261c571d2032ce2798122089ffedb0
DESKTOP-CJ9OI5G\Szejk <rafajk@wp.pl>
2017-06-26 19:55:34 +0200

    Dodanie do klasy CardGroup metody inicjującej wartości kart :-)

--- Blackjack/src/main/java/src/CardGroup.java
+++ Blackjack/src/main/java/src/CardGroup.java
@@ -1,6 +1,24 @@
 package src;
 
+import java.util.ArrayList;
+
 public class CardGroup {
+	
+	public ArrayList<Card> cards = new ArrayList<Card>(); // wstępna lista kart
+	
+	public void initFullDeck() {   // nadanie wartości kart
+		this.cards.clear();
+		String[] ranks = { "As", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Walet", "Dama", "Król" };
+		int[] rankValues = { 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10 };
+
+		String[] suits = { "Trefl", "Kier", "Karo", "Pik" };
+
+		for (int i = 0; i < ranks.length; i++) {
+			for (int j = 0; j < suits.length; j++) {
+				this.cards.add(new Card(ranks[i], suits[j], rankValues[i]));
+			}
+		}
+	}
 
 
 }
